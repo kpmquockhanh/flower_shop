46360f38e9b6cfb731f465cd6aad25d0d04456c5
khanhlq <kpmquockhanh@gmail.com>
2019-01-13 10:13:15 +0700

    Final

--- config/app.php
+++ config/app.php
@@ -158,7 +158,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-//        Barryvdh\Debugbar\ServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
         Khanhlq\Crawler\CrawlerServiceProvider::class
     ],
@@ -209,7 +208,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-//        'Debugbar' => Barryvdh\Debugbar\Facade::class,
         'Image' => Intervention\Image\Facades\Image::class,
         'Crawler' => \Khanhlq\Crawler\Crawler::class,
 
