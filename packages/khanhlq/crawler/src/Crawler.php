93a09dbdcc347009d2811c752af85b26609d9dc4
khanhlq <khanhlq@hblab.vn>
2019-03-15 19:15:11 +0700

    make crawler V2

--- packages/khanhlq/crawler/src/Crawler.php
+++ packages/khanhlq/crawler/src/Crawler.php
@@ -39,41 +39,44 @@ class Crawler
         $categories = Category::query()->get();
         $linkUrls = [];
 
-        foreach ($baseUrls as $baseUrl)
-        {
+        foreach ($baseUrls as $baseUrl) {
             $page = $this->getPage($baseUrl);
-            if (!$page) break;
+            if (!$page) {
+                break;
+            }
             //get link list category
-            $links = $this->getListBySelector($page,'div.r_nav ul li a');
+            $links = $this->getListBySelector($page, 'div.r_nav ul li a');
 //            dd($links);
-            foreach ($links as $link)
-            {
+            foreach ($links as $link) {
                 $slug = str_slug(trim($link->text()));
 
                 $category = $categories->where('cate_code', $slug)->first();
-                if ($category)
+                if ($category) {
                     $linkUrls[] = [
                         'link' => $this->getFullPath($link->attribute('href')),
                         'count' => CategoryFlower::with('flower')->where('category_id', $category->id)->count(),
                         'name' => $category->cate_name,
                     ];
-                else
+                } else {
                     $linkUrls[] = [
                         'link' => $this->getFullPath($link->attribute('href')),
                         'count' => 0,
                         'name' => $slug,
                     ];
+                }
             }
         }
 
         return $linkUrls;
     }
+
     public function crawl($index)
     {
-        if ($index == null)
+        if ($index == null) {
             return [
                 'status' => false
             ];
+        }
 
         ini_set('max_execution_time', 0);
         ini_set('memory_limit', '-1');
@@ -90,88 +93,89 @@ class Crawler
         $page = $this->getPage($baseUrl);
         try {
             $dom = $page->find('#ctl00_cphContent_hdfGroupId');
-        }catch(\Exception $e)
-        {
+        } catch (\Exception $e) {
             $dom = $page->find('#ctl00_cphContent_hdfKeyword');
         }
         $cateId = $dom->attribute('value');
 
 
-        if (is_numeric($cateId))
+        if (is_numeric($cateId)) {
             $url = 'https://hoayeuthuong.com/ajax/ProGroupHandler.ashx';
-        else
+        } else {
             $url = 'https://hoayeuthuong.com/ajax/ProKeywordHandler.ashx';
+        }
 
-
-        foreach ($this->getListBySelector($page, '.items .item .i a') as $flower)
-        {
+        foreach ($this->getListBySelector($page, '.items .item .i a') as $flower) {
             $data = $this->getData($flower->attribute('href'));
-            if (!isset($data['price'])) continue;
-            if ($data['price'] == 0) continue;
+            if (!isset($data['price'])) {
+                continue;
+            }
+            if ($data['price'] == 0) {
+                continue;
+            }
 
-            if (!$this->createFlower($data)) continue;
+            if (!$this->createFlower($data)) {
+                continue;
+            }
         }
 
         $pIndex = 1;
 
-        do
-        {
+        do {
             $crawlData = (
-                json_decode(
-                    $this->requestGetFlower(
-                        $url, '{"catId":"'.$cateId.'","pIndex":'.$pIndex++.',"orderQuery":"","lang":"vn"}'
-                    )
+            json_decode(
+                $this->requestGetFlower(
+                    $url,
+                    '{"catId":"' . $cateId . '","pIndex":' . $pIndex++ . ',"orderQuery":"","lang":"vn"}'
                 )
+            )
             );
 
-            foreach($crawlData->dataItem as $item)
-            {
+            foreach ($crawlData->dataItem as $item) {
                 //Detail
 
                 $data = ($this->getData($item->LinkItem));
 
-                if (!isset($data['price'] )) $data['price'] = 0;
+                if (!isset($data['price'])) {
+                    $data['price'] = 0;
+                }
 
-                if (!$this->createFlower($data)) continue;
+                if (!$this->createFlower($data)) {
+                    continue;
+                }
             }
-        }while($crawlData->dataItem);
+        } while ($crawlData->dataItem);
 
         return [
             'status' => true,
             'data' => $this->getCate(str_slug($itemLink['name']), $itemLink['link'])
         ];
-
     }
+
     public function createFlower($data)
     {
-        try{
-            if(!$flower = Flower::query()->where('slug', str_slug($data['name']))->first())
-            {
-                $this->downloadImage($data['img_link'],$data['image']);
+        try {
+            if (!$flower = Flower::query()->where('slug', str_slug($data['name']))->first()) {
+                $this->downloadImage($data['img_link'], $data['image']);
 //                dd($data);
                 $flower = Flower::query()->create($data);
-
             }
 
-            if(!$cate = Category::query()->where('cate_code', str_slug($data['category']))->first())
-            {
+            if (!$cate = Category::query()->where('cate_code', str_slug($data['category']))->first()) {
                 $cate = Category::query()->create([
                     'cate_name' => $data['category'],
                     'cate_code' => str_slug($data['category']),
                 ]);
             }
 
-            if(!CategoryFlower::query()->where('flower_id', $flower->id)->where('category_id', $cate->id)->first())
-            {
+            if (!CategoryFlower::query()->where('flower_id', $flower->id)->where('category_id', $cate->id)->first()) {
                 CategoryFlower::query()->create([
                     'flower_id' => $flower->id,
                     'category_id' => $cate->id,
                 ]);
             }
             return true;
-
-        }catch (\Exception $e)
-        {
+        } catch (\Exception $e) {
             return false;
         }
     }
@@ -182,33 +186,30 @@ class Crawler
 
         $catetogory = $categories->where('cate_code', $slug)->first();
 
-        if ($catetogory)
+        if ($catetogory) {
             return [
                 'link' => $link,
                 'count' => CategoryFlower::with('flower')->where('category_id', $catetogory->id)->count(),
                 'name' => $catetogory->cate_name,
             ];
-        else
+        } else {
             return [
                 'link' => $link,
                 'count' => 0,
                 'name' => "Chưa có",
             ];
+        }
     }
 
     public function getFullPath($url)
     {
         $baseUrl = $this->baseUrls[0];
-        if (!$this->isUrl($url))
-        {
+        if (!$this->isUrl($url)) {
             $regex = '/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})\//';
             $matches = [];
-            if (preg_match($regex, $baseUrl, $matches))
-            {
-                return $matches[0].($url[0]=='/'?substr($url,1):$url);
+            if (preg_match($regex, $baseUrl, $matches)) {
+                return $matches[0] . ($url[0] == '/' ? substr($url, 1) : $url);
             }
-
-
         }
         return null;
     }
@@ -222,23 +223,21 @@ class Crawler
     {
         Storage::disk('uploads')->put($name, file_get_contents($url));
     }
+
     public function getPage($baseURL)
     {
         try {
             $newPage = new Page();
             $newPage->visit($baseURL);
             return $newPage;
-        }catch(\Exception $e){
+        } catch (\Exception $e) {
             return null;
         }
     }
+
     public function getListBySelector(Page $html, string $selector)
     {
-        try {
-            return $html->all($selector);
-        }catch (\Exception $e){
-            dd(123123);
-        }
+        return $html->all($selector);
     }
 
     public function getData($link)
@@ -252,21 +251,26 @@ class Crawler
         $regex = '/(\d+\.?)+/';
         try {
             $domNew = $content->find('.l_item .b_item .single .new span');
-            if (preg_match($regex, $domNew->text(), $matches))
-                $data['sale_price'] = str_replace('.','',$matches[0]);
-        }catch (\Exception $e){}
+            if (preg_match($regex, $domNew->text(), $matches)) {
+                $data['sale_price'] = str_replace('.', '', $matches[0]);
+            }
+        } catch (\Exception $e) {
+        }
         try {
             $domOld = $content->find('.l_item .b_item .single .old');
-            if (preg_match($regex, $domOld->text(), $matches))
-                $data['price'] = str_replace('.','',$matches[0]);
-        }catch (\Exception $e){
+            if (preg_match($regex, $domOld->text(), $matches)) {
+                $data['price'] = str_replace('.', '', $matches[0]);
+            }
+        } catch (\Exception $e) {
             return [];
         }
 
         $data['saleoff'] = 0;
-        if (isset($data['sale_price']) && isset($data['price']))
-            if ($data['sale_price']!=0 && $data['price'] !=0)
-                $data['saleoff'] = round(1-($data['sale_price']/$data['price']), 2);
+        if (isset($data['sale_price']) && isset($data['price'])) {
+            if ($data['sale_price'] != 0 && $data['price'] != 0) {
+                $data['saleoff'] = round(1 - ($data['sale_price'] / $data['price']), 2);
+            }
+        }
 
         try {
             $data['name'] = explode(' - ', trim($content->find('.r_item h1')->text()))[1];
@@ -274,13 +278,15 @@ class Crawler
             $data['message'] = trim($content->find('.r_item .desc')->text());
             $data['slug'] = str_slug($data['name']);
             $data['quantity'] = 0;
-            $data['admin_id'] = Auth::guard('admin')->id()??1;
+            $data['admin_id'] = Auth::guard('admin')->id() ?? 1;
             $data['show'] = 1;
-            if (!isset($data['price'])) $data['price'] = 0;
+            if (!isset($data['price'])) {
+                $data['price'] = 0;
+            }
             $data['img_link'] = $content->find('.l_item .t_item img')->attribute('data-original');
-            $imgInfo =pathinfo($data['img_link']);
-            $data['image'] = time().'_'.$imgInfo['filename'].'.'.$imgInfo['extension'];
-        }catch(\Exception $e){
+            $imgInfo = pathinfo($data['img_link']);
+            $data['image'] = time() . '_' . $imgInfo['filename'] . '.' . $imgInfo['extension'];
+        } catch (\Exception $e) {
             return [];
         }
 
@@ -292,7 +298,7 @@ class Crawler
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS,  $data);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
         $output = curl_exec($ch);
         curl_close($ch);
 
--- packages/khanhlq/crawler/src/CrawlerV2.php
+++ packages/khanhlq/crawler/src/CrawlerV2.php
@@ -8,28 +8,172 @@
 
 namespace Khanhlq\Crawler;
 
-use Khanhlq\Crawler\Step;
+use Openbuildings\Spiderling\Node;
 use Openbuildings\Spiderling\Page;
-
 
 class CrawlerV2
 {
+
+    /**
+     * @var string $path
+     */
+    private $path;
+
     private $crawler;
 
-    private $path;// Array of steps
 
-
-
-    public function __construct($path)
+    public function __construct($path, Page $page)
     {
-        $this->crawler = new Page();
         $this->path = $path;
+        $this->crawler = $page;
     }
 
 
-    public function crawl($rootUrl)
+    public function crawl()
     {
+        //
+        $this->path = [
+            'root' => 'https://hoayeuthuong.com/',
+            'list_link' => [1 => 'div.r_nav ul li a'],
+            'fields' => [
+                'title'
+            ],
+        ];
 
+        $rootPage = $this->getPage($this->path['root']);
     }
+
+    /**
+     * Get all of list item to get detail page
+     * @param Page $page
+     * @param $selector
+     * @return array
+     */
+    public function getListLinkSimple(Page $page, $selector)
+    {
+        $arrLinks = [];
+        foreach ($page->all($selector) as $item) {
+            $arrLinks[] = $this->getAttribute($item, 'href');
+        }
+        return $arrLinks;
+    }
+
+    /**
+     * @param Page $page
+     * @param string $selector
+     * @param string $nextBtnSelector
+     * @param string $attrNextButton
+     * @return array
+     */
+    public function getListLinkPaginate(
+        Page $page,
+        string $selector,
+        string $nextBtnSelector,
+        string $attrNextButton = 'href'
+    ) {
+        $arrLinks = $this->getListLinkSimple($page, $selector);
+
+        while ($nextLinkNode = $page->find($nextBtnSelector)) {
+            $nextLink = $this->getAttribute($nextLinkNode, $attrNextButton);
+            array_merge($arrLinks, $this->getListLinkSimple($page->visit($nextLink), $selector));
+        }
+        return $arrLinks;
+    }
+
+    /**
+     * @param Node $node
+     * @return string
+     */
+    public function getText(Node $node)
+    {
+        return $node->text();
+    }
+
+    /**
+     * @param Node $node
+     * @param string $attr
+     * @return string
+     */
+    public function getAttribute(Node $node, string $attr)
+    {
+        return $node->attribute($attr);
+    }
+
+    /**
+     * @param Node $node
+     * @return string
+     */
+    public function getHtml(Node $node)
+    {
+        return $node->html();
+    }
+
+    /**
+     * @param $url
+     * @return Page
+     */
+    public function getPage($url)
+    {
+        $page = new Page();
+        $page->visit($url);
+        return $page;
+    }
+
+    /**
+     * @param Page $page
+     * @param string $name
+     * @param string $selector
+     * @param int $type
+     * @param string $attr
+     * @param bool $isMultiple
+     * @param string $glue
+     */
+    public function getValue(
+        Page $page,
+        string $name,
+        string $selector,
+        int $type, // 1: text, 2: attribute, 3:html
+        string $attr,
+        bool $isMultiple = false,
+        string $glue = " "
+    ) {
+        $data = [];
+
+        if ($isMultiple) {
+            $elements = $page->all($selector);
+            $result = '';
+            foreach ($elements as $index => $item) {
+                $endGlue = ($index < count($elements) -1)?$glue:'';
+                switch ($type) {
+                    case 1:
+                        $result .= $this->getText($item).$endGlue;
+                        break;
+                    case 2:
+                        $result .= $this->getAttribute($item, $attr).$endGlue;
+                        break;
+                    case 3:
+                        $result = $this->getHtml($item);
+                        break;
+                }
+            }
+        } else {
+            $result = '';
+            $element = $page->find($selector);
+            switch ($type) {
+                case 1:
+                    $result = $this->getText($element);
+                    break;
+                case 2:
+                    $result = $this->getAttribute($element, $attr);
+                    break;
+                case 3:
+                    $result = $this->getHtml($element);
+                    break;
+            }
+        }
+
+        $data[$name] = $result;
+    }
+
 
 }
